6cb06607c3257d74425d535d6868a20ae8ce64ef
David Ghazi <davidaghazi@gmail.com>
2020-05-11 00:27:44 -0400

    updated ID

--- routes/token-api-routes.js
+++ routes/token-api-routes.js
@@ -6,8 +6,8 @@ let app = express()
 
 let PORT = 8888;
 
-const SPOTIFY_CLIENT_ID = '24fbfc849b2a46f792a66d56d872ef90'
-const SPOTIFY_CLIENT_SECRET = '588bae3a74e54732ae52026bf86257bc'
+const SPOTIFY_CLIENT_ID = '96cba94b0188420d9b0947302e101419'
+const SPOTIFY_CLIENT_SECRET = 'd6d4076155934e91b382b9d192b9bd3d'
 
 let redirect_uri = 
   process.env.REDIRECT_URI || 
